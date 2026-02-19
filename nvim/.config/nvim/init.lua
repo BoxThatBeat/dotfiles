f70ba3fa9f7539621d3ff9967528f1428818a085
aaron <aaron@jsisensors.ca>
2026-02-19 14:00:57 -0500

    Scroll less and center on ctrl-d and ctrl-u

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -9,3 +9,15 @@ vim.keymap.set("n", "<X1Mouse>", "<C-o>", { desc = "Jump back" })
 
 -- Map mouse forward button to Ctrl+I (jump forward)
 vim.keymap.set("n", "<X2Mouse>", "<C-i>", { desc = "Jump forward" })
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+local function set_scroll()
+	vim.opt_local.scroll = math.max(1, math.floor(vim.api.nvim_win_get_height(0) / 4))
+end
+
+set_scroll()
+vim.api.nvim_create_autocmd({ "VimResized", "WinEnter" }, {
+	callback = set_scroll,
+})
