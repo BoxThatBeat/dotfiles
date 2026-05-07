ddccd5a2b63bca7361f120dca5db620f0fecc1da
aaron <aaron@jsisensors.ca>
2026-05-07 14:35:08 -0400

    debugging remap

--- nvim/.config/nvim/lua/plugins/dap-js.lua
+++ nvim/.config/nvim/lua/plugins/dap-js.lua
@@ -46,9 +46,9 @@ return {
     "mfussenegger/nvim-dap",
     keys = {
       { "<F5>", function() require("dap").continue() end, desc = "Continue" },
-      { "<F10>", function() require("dap").step_over() end, desc = "Step Over" },
-      { "<F11>", function() require("dap").step_into() end, desc = "Step Into" },
-      { "<F12>", function() require("dap").step_out() end, desc = "Step Out" },
+      { "<F6>", function() require("dap").step_into() end, desc = "Step Into" },
+      { "<F7>", function() require("dap").step_over() end, desc = "Step Over" },
+      { "<F8>", function() require("dap").step_out() end, desc = "Step Out" },
     },
     opts = function()
       local dap = require("dap")
