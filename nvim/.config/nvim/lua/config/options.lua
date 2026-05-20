b8b9ec4fb2b880c386490e92a9707f1a2f16c93b
aaron <aaron@jsisensors.ca>
2026-05-20 14:21:48 -0400

    feat: scope better in nvim

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -4,3 +4,8 @@
 
 -- Use system clipboard for all yank/delete/paste operations
 vim.opt.clipboard = "unnamedplus"
+
+-- Always treat the directory nvim was launched in as the project root.
+-- Default LazyVim spec is { "lsp", { ".git", "lua" }, "cwd" }, which makes
+-- pickers/explorer jump into a submodule's .git when you open a file inside it.
+vim.g.root_spec = { "cwd" }
